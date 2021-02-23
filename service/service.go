e3dbea42819f2fbab4e1badd20ceeeabe6fb040b
Zachariah Ngonyani <zachariahn@gmail.com>
2021-02-23 09:22:07 +0300

    Add service package

---
+++ service/service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Start starts the service
+func Start(ctx context.Context, serviceName, host, port string, registerHandlersFunc func()) (context.Context, error) {
+	registerHandlersFunc()
+	ctx = startService(ctx, serviceName, host, port)
+
+	return ctx, nil
+}
+
+func startService(ctx context.Context, serviceName, host, port string) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
+
+	var srv http.Server
+	srv.Addr = ":" + port
+
+	go func() {
+		log.Panicln(srv.ListenAndServe())
+		cancel()
+	}()
+
+	go func() {
+		fmt.Printf("%v started. Press any key to stop.\n", serviceName)
+		var s string
+		fmt.Scanln(&s)
+		srv.Shutdown(ctx)
+		cancel()
+	}()
+
+	return ctx
+}
